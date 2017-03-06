b4c905d9dfc1c6ffc97454b4e298c2aa4172d040
Anna D Derbakova <hiopa16@gmail.com>
2017-03-06 01:01:04 -0500

    Customer application is functional
    The customer is now able to select which item ID they would like to
    purchase. The logic then checks whether the quantity requested by the
    customer is available. If so, the order is fulfilled and the product
    quantity is updated in the database. If the customer has requested a
    quantity that is not available, they are asked to modify their order
    before it can be processed.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,6 +15,18 @@ var connection = mysql.createConnection({
 	database: 'Bamazon'
 });
 
+// validateInput makes sure that the user is supplying only positive integers for their inputs
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
+
 // promptUserPurchase will prompt the user for the item/quantity they would like to purchase
 function promptUserPurchase() {
 	// console.log('___ENTER promptUserPurchase___');
@@ -24,26 +36,70 @@ function promptUserPurchase() {
 		{
 			type: 'input',
 			name: 'item_id',
-			message: 'Please enter the Item ID which you would like to purchase.'
+			message: 'Please enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
 		},
 		{
 			type: 'input',
 			name: 'quantity',
 			message: 'How many do you need?',
-			validate: function (value) {
-				var integer = Number.isInteger(parseFloat(value));
-				var sign = Math.sign(value);
-
-				if (integer && (sign === 1)) {
-					return true;
-				} else {
-					return 'Please enter a whole non-zero number.';
-				}
-			},
+			validate: validateInput,
 			filter: Number
 		}
 	]).then(function(input) {
-		console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+		// console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		// Query db to confirm that the given item ID exists in the desired quantity
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			// If the user has selected an invalid item ID, data attay will be empty
+			// console.log('data = ' + JSON.stringify(data));
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				// console.log('productData = ' + JSON.stringify(productData));
+				// console.log('productData.stock_quantity = ' + productData.stock_quantity);
+
+				// If the quantity requested by the user is in stock
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+					// console.log('updateQueryStr = ' + updateQueryStr);
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						// End the database connection
+						connection.end();
+					})
+				} else {
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
 	})
 }
 
@@ -76,8 +132,6 @@ function displayInventory() {
 
 	  	//Prompt the user for item/quantity they would like to purchase
 	  	promptUserPurchase();
-
-	  	connection.end();
 	})
 }
 
